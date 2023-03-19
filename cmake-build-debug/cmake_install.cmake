22a0a711e1caebd1e12e4616f53fbb69b7db3fb2
zhoujunlingla <2966279127@qq.com>
2023-03-19 17:34:11 +0800

    Assignment1 - 2 区域填充

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/zhoujunl/CLionProjects/open_gl_install
+# Install script for directory: /Users/zhoujunl/CLionProjects/Assignment1 - 2
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/zhoujunl/CLionProjects/open_gl_install/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/zhoujunl/CLionProjects/Assignment1 - 2/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
