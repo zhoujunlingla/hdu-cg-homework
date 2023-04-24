bf48a604884ee147ea340dcab7a979abf0d48e93
zhoujunlingla <2966279127@qq.com>
2023-04-24 22:17:24 +0800

    完成assignment1

--- src/main.cpp
+++ src/main.cpp
@@ -1,56 +1,96 @@
 #include <GL/glut.h>
-#include <stack>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+// 定义一个表示点的结构体
+struct Point {
+    int x, y;
+};
 
-using namespace std;
+// 存储多边形顶点的向量
+std::vector<Point> polygon_vertices;
 
-void regionFill(int x, int y, float* fillColor, float* bgColor) {
-    stack<pair<int, int>> s;
-    s.push(make_pair(x, y));
-    while (!s.empty()) {
-        pair<int, int> p = s.top();
-        s.pop();
-        int i = p.first, j = p.second;
-        float pixelColor[3];
-        glReadPixels(i, j, 1, 1, GL_RGB, GL_FLOAT, pixelColor);
-        if (pixelColor[0] == bgColor[0] && pixelColor[1] == bgColor[1] && pixelColor[2] == bgColor[2]) {
-            glColor3fv(fillColor);
-            glBegin(GL_POINTS);
-            glVertex2i(i, j);
-            glEnd();
-            s.push(make_pair(i+1, j));
-            s.push(make_pair(i-1, j));
-            s.push(make_pair(i, j+1));
-            s.push(make_pair(i, j-1));
-        }
-    }
+// 比较函数，根据y值对点进行排序
+bool compareY(Point a, Point b) {
+    return a.y < b.y;
 }
 
+// 初始化OpenGL
+void init() {
+    glClearColor(0.0, 0.0, 0.0, 1.0);
+    glMatrixMode(GL_PROJECTION);
+    gluOrtho2D(0, 640, 0, 480);
+}
+
+// 显示回调函数
 void display() {
-    glClearColor(1.0, 1.0, 1.0, 1.0);
     glClear(GL_COLOR_BUFFER_BIT);
-    glBegin(GL_POLYGON);
-    glColor3f(1.0, 0.0, 0.0);
-    glVertex2f(0.0, 0.0);
-    glVertex2f(0.0, 100.0);
-    glVertex2f(100.0, 100.0);
-    glVertex2f(100.0, 0.0);
+    glColor3f(1.0, 1.0, 1.0);
+
+    // 绘制多边形
+    glBegin(GL_LINE_LOOP);
+    for (auto vertex : polygon_vertices) {
+        glVertex2i(vertex.x, vertex.y);
+    }
     glEnd();
+
+    // 对多边形顶点按照y值进行排序
+    std::sort(polygon_vertices.begin(), polygon_vertices.end(), compareY);
+
+    // 扫描线算法
+    for (int current_y = polygon_vertices.front().y; current_y <= polygon_vertices.back().y; ++current_y) {
+        std::vector<int> intersections;
+
+        // 查找交点
+        for (size_t i = 0, j = polygon_vertices.size() - 1; i < polygon_vertices.size(); j = i++) {
+            if ((polygon_vertices[i].y > current_y) != (polygon_vertices[j].y > current_y)) {
+                int intersection_x = (polygon_vertices[i].x + (current_y - polygon_vertices[i].y) * (polygon_vertices[j].x - polygon_vertices[i].x) / (polygon_vertices[j].y - polygon_vertices[i].y));
+                intersections.push_back(intersection_x);
+            }
+        }
+
+        // 对交点进行排序
+        std::sort(intersections.begin(), intersections.end());
+
+        // 绘制扫描线
+        glBegin(GL_LINES);
+        for (size_t i = 0; i < intersections.size(); i += 2) {
+            glVertex2i(intersections[i], current_y);
+            glVertex2i(intersections[i + 1], current_y);
+        }
+        glEnd();
+    }
+
     glFlush();
-    float fillColor[] = {0.0, 1.0, 0.0};
-    float bgColor[] = {1.0, 1.0, 1.0};
-    regionFill(50, 50, fillColor, bgColor);
 }
 
 int main(int argc, char** argv) {
+    // 输入多边形顶点
+    int n;
+    std::cout << "请输入多边形顶点数: ";
+    std::cin >> n;
+    std::cout << "请依次输入多边形顶点（x, y）: " << std::endl;
+
+    for (int i = 0; i < n; ++i) {
+        Point vertex;
+        std::cin >> vertex.x >> vertex.y;
+        polygon_vertices.push_back(vertex);
+    }
+
+    // 初始化
+    // 初始化GLUT并设置窗口属性
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-    glutInitWindowSize(400, 400);
+    glutInitWindowSize(640, 480);
     glutInitWindowPosition(100, 100);
-    glutCreateWindow("Region Fill");
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    gluOrtho2D(0.0, 400.0, 0.0, 400.0);
+    glutCreateWindow("多边形扫描转换算法");
+
+    // 设置OpenGL回调函数
+    init();
     glutDisplayFunc(display);
+
+    // 进入GLUT事件处理循环
     glutMainLoop();
+
     return 0;
 }
