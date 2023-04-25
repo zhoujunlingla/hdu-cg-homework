e7c34437e269c051919824e1bd769136082c5876
zhoujunlingla <2966279127@qq.com>
2023-04-25 10:58:36 +0800

    完成assignment1，2，3。

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/zhoujunl/CLionProjects/Assignment1
+# Install script for directory: /Users/zhoujunl/CLionProjects/cg/Assignment1
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/zhoujunl/CLionProjects/Assignment1/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/zhoujunl/CLionProjects/cg/Assignment1/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
